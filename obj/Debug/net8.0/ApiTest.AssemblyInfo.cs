fc930419f4d4fd598986d85dff528ccf2be95fcf
Kaung Khant <kkayeminaca@gmail.com>
2024-07-05 13:18:49 +0630

    Added gitignore file

--- obj/Debug/net8.0/ApiTest.AssemblyInfo.cs
+++ obj/Debug/net8.0/ApiTest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+718fdfc0d6822b0cb66ef95c192f983ea1321dc1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
